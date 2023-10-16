5c31bacd2be722d1590f0bf2c2fc71959d78d5f6
lizhang0331 <lickyzl0331@yahoo.com>
2023-10-15 21:44:41 -0400

    frame of the project

---
+++ src/test/java/com/li/petit/PetitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.petit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
